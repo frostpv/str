1610dbe3cc230b42febecdbdb62eb2b69011d7b7
vasilijpetecuk <frostpv@gmail.com>
2023-06-25 13:33:04 +0300

    [BE][TSK-6] - init commit

---
+++ src/test/java/com/example/ServiceSTR/ServiceStrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServiceSTR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceStrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
