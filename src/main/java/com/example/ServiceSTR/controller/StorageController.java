8bfc918732748ae342593b34c2b70674778c8e08
vasilijpetecuk <frostpv@gmail.com>
2023-07-09 11:49:11 +0300

    [BE][TSK-6] - Added doker, storages repository, new db connection

--- src/main/java/com/example/ServiceSTR/controller/StorageController.java
+++ src/main/java/com/example/ServiceSTR/controller/StorageController.java
@@ -6,12 +6,7 @@ import com.example.ServiceSTR.service.StorageService;
 import com.example.ServiceSTR.util.StorageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,7 +19,7 @@ public class StorageController {
     private StorageService storageService;
 
     @PostMapping
-    public ResponseEntity<StorageDto> addStorage(@RequestParam StorageDto storageDto){
+    public ResponseEntity<StorageDto> addStorage(@RequestBody StorageDto storageDto){
         StorageEntity storageEntity = storageService.addStorage(storageDto);
 
         return ResponseEntity.ok(StorageConverter.toDto(storageEntity));
